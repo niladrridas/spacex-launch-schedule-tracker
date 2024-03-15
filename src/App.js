d18778bf3a8ff218e39f470d688f896649343607
Niladri Das <niladri.das@lpu.in>
2024-03-16 01:21:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>SpaceX Launch Schedule Tracker</h1>
+      {/* Add components and routes here */}
+    </div>
+  );
+}
+
+export default App;
